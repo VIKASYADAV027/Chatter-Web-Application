0affb38b9dcd3de661c489a6a7fa5bf2da2c869f
Vikas Yadav <vikasya12118@gmail.com>
2025-06-28 18:53:17 +0530

    commiting changes after making to same page

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,13 @@ import 'dotenv/config';
 import connectDB from './lib/db.js';
 import cookieParser from 'cookie-parser'
 import cors from 'cors';
+import path from 'path';
 
 
 const PORT = process.env.PORT || 5001;
+
+const __dirname = path.resolve();
+
 app.use(express.json({limit: '10mb'}));
 app.use(cookieParser({limit: '10mb', extended: true}));
 // Middleware to enable CORS
@@ -22,6 +26,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*", (req, res) => {
+     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 server.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
